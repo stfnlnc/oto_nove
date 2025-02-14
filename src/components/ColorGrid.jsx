a4f2bad7be0d1792366b6ee98121c2fe4868f042
Stefan <stefan@studiokhi.com>
2025-02-14 15:41:15 +0100

    feat: add content

--- src/components/ColorGrid.jsx
+++ src/components/ColorGrid.jsx
@@ -74,8 +74,42 @@ export default function ColorGrid() {
                         <div className="relative text-xl -rotate-90 mt-12">Friday</div>
                         <div className="pointer-events-auto absolute top-0 left-full w-screen h-[100lvh] grid grid-cols-17">
                             <div className="content col-span-12 bg-white text-black p-5 h-[100lvh] overflow-y-scroll">
-                                <div className="w-full h-[200lvh]">
-                                    <h1 className="text-9xl">Friday</h1>
+                                <div className="w-full flex flex-col items-start justify-start">
+                                    <h2 className="galgo text-[25vw] w-full flex flex-row items-start justify-between leading-[0.6] pt-5 pb-5 border-b border-black">
+                                        <span>01.25.</span>
+                                        <span>2025</span>
+                                    </h2>
+                                    <div className="grid grid-cols-2 py-5 w-full">
+                                        <div className="flex flex-col">
+                                            <div className="flex flex-row gap-2 uppercase text-[2.4vw] items-center justify-start leading-[1.2]">
+                                                <svg className="w-[2.4vw]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 36" fill="none">
+                                                    <path d="M18.3203 8.15625L28.1641 18L18.3203 27.8438M26.7969 18H6.83594" stroke="#262626" strokeWidth="2.5" strokeMiterlimit="10" strokeLinecap="square"/>
+                                                </svg>
+                                                Buy Tickets
+                                            </div>
+                                            <div className="flex flex-row gap-2 uppercase text-[2.4vw] items-center justify-start leading-[1.2]">
+                                                <svg className="w-[2.4vw]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 36" fill="none">
+                                                    <path d="M18.3203 8.15625L28.1641 18L18.3203 27.8438M26.7969 18H6.83594" stroke="#262626" strokeWidth="2.5" strokeMiterlimit="10" strokeLinecap="square"/>
+                                                </svg>
+                                                Buy Pass
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col w-full">
+                                            <div className="border-b border-black w-full uppercase text-[2.4vw] leading-[1.2]">
+                                                les instants chavirés
+                                            </div>
+                                            <div className="border-b border-black w-full uppercase text-[2.4vw] leading-[1.2]">
+                                                7, rue richard lenoir
+                                            </div>
+                                            <div className="border-b border-black w-full uppercase text-[2.4vw] leading-[1.2]">
+                                                93100 montreuil
+                                            </div>
+                                            <p className="text-[1.875vw] normal-case pt-5 leading-[1.2]">
+                                                Experience an evening where sound becomes a living, breathing entity. Accompanied by contemplative light projections, this night transforms the room into an immersive canvas of sights and sounds.
+                                            </p>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
                             <div className="col-span-5"></div>
@@ -85,8 +119,11 @@ export default function ColorGrid() {
                         <div className="text-xl -rotate-90 mt-16">Saturday</div>
                         <div className="pointer-events-auto absolute top-0 left-full w-screen h-[100lvh] grid grid-cols-17">
                             <div className="content col-span-12 bg-white text-black p-5 h-[100lvh] overflow-y-scroll">
-                                <div className="w-full h-[200lvh]">
-                                    <h1 className="text-9xl">Saturday</h1>
+                                <div className="w-full flex flex-col items-start justify-start">
+                                    <h2 className="galgo text-[25vw] w-full flex flex-row items-start justify-between leading-[0.8]">
+                                        <span>01.26.</span>
+                                        <span>2025</span>
+                                    </h2>
                                 </div>
                             </div>
                             <div className="col-span-5"></div>
@@ -96,8 +133,11 @@ export default function ColorGrid() {
                         <div className="text-xl -rotate-90 mt-14">Sunday</div>
                         <div className="pointer-events-auto absolute top-0 left-full w-screen h-[100lvh] grid grid-cols-17">
                             <div className="content col-span-12 bg-white text-black p-5 h-[100lvh] overflow-y-scroll">
-                                <div className="w-full h-[200lvh]">
-                                    <h1 className="text-9xl">Sunday</h1>
+                                <div className="w-full flex flex-col items-start justify-start">
+                                    <h2 className="galgo text-[25vw] w-full flex flex-row items-start justify-between leading-[0.8]">
+                                        <span>01.27.</span>
+                                        <span>2025</span>
+                                    </h2>
                                 </div>
                             </div>
                             <div className="col-span-5"></div>
